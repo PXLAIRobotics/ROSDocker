6e9ae4dd176bbdac25900353f7de226a82f4e874
Tim Dupont <tim.dupont@pxl.be>
2020-03-03 23:28:51 +0100

    Added signals slots example.

---
+++ ExampleCode/gui/signals_and_slots.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QPushButton
+
+def window():
+    app = QApplication(sys.argv)
+    win = QDialog()
+    b1 = QPushButton(win)
+    b1.setText("Button1")
+    b1.move(50,20)
+    b1.clicked.connect(b1_clicked)
+
+    b2 = QPushButton(win)
+    b2.setText("Button2")
+    b2.move(50,50)
+    # The line below is PyQt4 code. It doesn't work in PyQt5.
+    #QObject.connect(b2,SIGNAL("clicked()"),b2_clicked)
+    b2.clicked.connect(b2_clicked)
+
+    win.setGeometry(100,100,200,100)
+    win.setWindowTitle("PyQt")
+    win.show()
+    sys.exit(app.exec_())
+
+def b1_clicked():
+    print "Button 1 clicked"
+
+def b2_clicked():
+    print "Button 2 clicked"
+
+if __name__ == '__main__':
+    window()
